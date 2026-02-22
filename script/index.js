519e0661e130858cebc87efd6bd13d68615c2f82
Priyotush <priyotushpaul40@gmail.com>
2026-02-22 23:06:10 +0600

    add categorywise product data show

--- script/index.js
+++ script/index.js
@@ -1,67 +1,88 @@
 const categoryContainer = document.getElementById("category-container");
 const productContainer = document.getElementById("product-container");
 
-const loadCategory = () =>{
+// category
+const loadCategory = () => {
   fetch("https://fakestoreapi.com/products/categories")
-  .then(res => res.json())
-  .then(data => displayCategory(data))
-}
+    .then((res) => res.json())
+    .then((data) => displayCategory(data));
+};
 
 loadCategory();
 
-const displayCategory = (category) =>{
-  category.forEach(element => {
+const displayCategory = (category) => {
+  category.forEach((element) => {
     categoryContainer.innerHTML += `
     
     <p class="btn btn-outline font-medium">${element}</p>
-    
-    `
+ 
+
+    `;
   });
-}
+};
 
-const loadProducts = () =>{
+// product
+const loadProducts = () => {
   fetch("https://fakestoreapi.com/products")
-  .then(res => res.json())
-  .then(data => displayProducts(data))
-
-}
+    .then((res) => res.json())
+    .then((data) => displayProducts(data));
+};
 
 loadProducts();
 
 const displayProducts = (products) => {
-  products.forEach(product => {
+  productContainer.innerHTML = "";
 
-    console.log(product);
-
+  products.forEach((product) => {
     productContainer.innerHTML += `
     
-<div class="rounded shadow-sm border border-gray-50">
+      <div class="rounded shadow-sm border border-gray-50">
+          
+        <div class="rounded bg-gray-200 p-4 flex items-center justify-center">
+          <img src="${product.image}" class="h-80 object-cover" />
+        </div>
+
+      <div class="p-4 space-y-4">
+
+        <div class="flex items-center justify-between">
+          <span class="text-blue-600">${product.category}</span>
+          <p class="text-gray-500">${product.rating.rate} <span>(${product.rating.count})</span></p>
+        </div>
+
+        <div>
+          <h3 class="text-xl font-medium text-gray-600">${product.title}</h3>
+          <p class="text-xl font-semibold mt-4">$ ${product.price}</p>
+        </div>
+
+        <div class="flex items-center justify-between">
+          <button class="btn">Details</button>
+          <button class="btn btn-primary text-white">Add</button>
+        </div>
+      </div>
+      </div>
     
-  <div class="rounded bg-gray-200 p-4 flex items-center justify-center">
-    <img src="${product.image}" class="h-80 object-cover" />
-  </div>
+    `;
+  });
+};
 
-<div class="p-4 space-y-4">
+// product by category
+categoryContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "p") {
+    const category = e.target.innerText;
+    console.log(category);
+    if (category === "All") {
+      loadProducts();
+    } else {
+      loadProductsByCategory(category);
+    }
+  }
+});
 
-  <div class="flex items-center justify-between">
-    <span class="text-blue-600">${product.category}</span>
-    <p class="text-gray-500">${product.rating.rate} <span>(${product.rating.count})</span></p>
-  </div>
-
-  <div>
-    <h3 class="text-xl font-medium text-gray-600">${product.title}</h3>
-    <p class="text-xl font-semibold mt-4">$ ${product.price}</p>
-  </div>
-
-  <div class="flex items-center justify-between">
-    <button class="btn">Details</button>
-    <button class="btn btn-primary text-white">Add</button>
-  </div>
-</div>
-
-</div>
-    
-    `
-  })
-
-}
+const loadProductsByCategory = (category) => {
+  fetch(`https://fakestoreapi.com/products/category/${category}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      displayProducts(data);
+    });
+};
