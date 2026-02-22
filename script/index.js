7de12170e4f3910071c81c437c40c059fcb5ec04
Priyotush <priyotushpaul40@gmail.com>
2026-02-22 19:44:02 +0600

    load category on api

--- script/index.js
+++ script/index.js
@@ -1,24 +1,10 @@
-const loadCategory = () => {
+const categoryContainer = document.getElementById("category-container");
+const productContainer = document.getElementById("product-container");
+
+const loadcateory = () =>{
   fetch("https://fakestoreapi.com/products/categories")
-    .then((res) => res.json())
-    .then((data) => displayCategory(data));
-};
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
 
-const displayCategory = (levels) => {
-  const levelContainer = document.getElementById("container");
-  levelContainer.innerHTML = "";
-
-  levels.forEach((level) => {
-    const btnDiv = document.createElement("div");
-
-    btnDiv.innerHTML = `
-        <button class="btn text-xl btn-outline outline-1 px-3 rounded-full">
-            ${level}
-        </button>
-        `;
-
-    levelContainer.appendChild(btnDiv);
-  });
-};
-
-loadCategory();
+loadcateory();
